6832fc375a7418a17a2762f3bdea348d056d5aa5
juniorcsharp <pikodellamirandola@gmail.com>
2016-06-22 09:11:36 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FieldGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FieldGenerator generator = new FieldGenerator();
+            generator.Width = 9;
+            generator.Height = 9;
+
+            try
+            {
+                string[,] massPole = generator.СreateField();
+                for (int i = 0; i < massPole.GetLength(0); i++)
+                {
+                    for (int y = 0; y < massPole.GetLength(1); y++)
+                    {
+                        Console.Write(massPole[i, y] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
